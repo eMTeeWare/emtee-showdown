dbc673c8633447e11672d715f9bb2536ae7474ad
Merlin Thomas <dev@emteeware.net>
2020-04-19 14:14:19 +0200

    Add logging

--- src/main/kotlin/net/emteeware/Selection.kt
+++ src/main/kotlin/net/emteeware/Selection.kt
@@ -1,16 +1,21 @@
 package net.emteeware
 
+
+import org.slf4j.LoggerFactory
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class Selection {
+    private val logger = LoggerFactory.getLogger(javaClass)
     val selectedSeasons = ArrayList<String>()
 
     fun addSeason(id : String) {
         selectedSeasons.add(id)
+        logger.info("Added season $id")
     }
 
     fun removeSeason(id: String) {
         selectedSeasons.remove(id)
+        logger.info("Removed season $id")
     }
 }

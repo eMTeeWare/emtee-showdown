2f21c326fe49c751b9e2643dd1dd9410817ca7e7
Merlin Thomas <dev@emteeware.net>
2020-04-19 12:05:37 +0200

    Post selection of season to server

--- src/main/resources/META-INF/resources/javascript.js
+++ src/main/resources/META-INF/resources/javascript.js
@@ -8,6 +8,21 @@ function select(id) {
         theElement.classList.remove("selected");
     } else {
         theElement.classList.add("selected");
+        var data = "id=" + theElement.id;
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("POST", "http://localhost:8080/selection");
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+        xhr.send(data);
     }
 }
 

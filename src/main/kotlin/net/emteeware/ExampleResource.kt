7f5f3d5e484b50fff204925e230d7bb8dc1cfe3f
Merlin Thomas <dev@emteeware.net>
2020-04-20 20:48:02 +0200

    Remove sample code from controller

--- src/main/kotlin/net/emteeware/ExampleResource.kt
+++ src/main/kotlin/net/emteeware/ExampleResource.kt
@@ -14,20 +14,9 @@ import javax.ws.rs.PUT
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType.*
 import javax.ws.rs.core.Response
 
-@Path("/hello")
-class ExampleResource {
-
-    @Inject
-    lateinit var hello: Template
-
-    @GET
-    @Produces(TEXT_HTML)
-    fun hello(@QueryParam("name") name: String?): TemplateInstance = hello.data("name", name ?: "Quarkus")
-}
 
 @Path("/selection")
 class SelectionResource {

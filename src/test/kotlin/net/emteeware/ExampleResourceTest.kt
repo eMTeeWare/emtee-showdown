d0b1db862e6928ea587bc06c4818a958bcbf18c7
Merlin Thomas <emteeware@gmail.com>
2020-05-26 19:23:27 +0200

    Make test pass

--- src/test/kotlin/net/emteeware/ExampleResourceTest.kt
+++ src/test/kotlin/net/emteeware/ExampleResourceTest.kt
@@ -2,7 +2,7 @@ package net.emteeware
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
+import org.apache.http.HttpStatus
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -13,8 +13,7 @@ class ExampleResourceTest {
         given()
             .`when`().get("/hello")
             .then()
-            .statusCode(200)
-            .body(`is`("hello"))
+            .statusCode(HttpStatus.SC_NOT_FOUND)
     }
 
 }

6d3636007e52fbeeac28ca2ed15e954e0d5456cd
Merlin Thomas <emteeware@gmail.com>
2020-06-06 13:20:40 +0200

    [#41] use existing shows as samples when starting the service

--- src/main/kotlin/net/emteeware/SeasonChoice.kt
+++ src/main/kotlin/net/emteeware/SeasonChoice.kt
@@ -11,12 +11,12 @@ class SeasonChoice {
     init {
         legacySeasonList.add(LegacySeason("How to Get Away with Murder", 3, 6, Year.of(2016), 22, LocalDate.parse("2020-03-08"), "100"))
         legacySeasonList.add(
-            LegacySeason("Marvel's Agents of S.H.I.E.L.D.", 5, 7, Year.of(2017), 15, LocalDate.parse("2020-03-08"), "200"))
-        legacySeasonList.add(LegacySeason("You", 2, 3, Year.of(2019), 10, LocalDate.parse("2020-02-18"), "300"))
-        legacySeasonList.add(LegacySeason("An enormously long running show with a rediculously long title", 128, 1024, Year.of(2019), 594, LocalDate.parse("2020-02-18"), "400"))
-        legacySeasonList.add(LegacySeason("You", 2, 3, Year.of(2019), 10, LocalDate.parse("2020-02-18"), "500"))
-        legacySeasonList.add(LegacySeason("You", 2, 3, Year.of(2019), 10, LocalDate.parse("2020-02-18"), "600"))
-        legacySeasonList.add(LegacySeason("You", 2, 3, Year.of(2019), 10, LocalDate.parse("2020-02-18"), "700"))
+            LegacySeason("Battlestar Galactica", 4, 4, Year.of(2008), 20, LocalDate.parse("2020-03-08"), "200"))
+        legacySeasonList.add(LegacySeason("Captain Future", 1, 14, Year.of(1978), 52, LocalDate.parse("2020-02-18"), "300"))
+        legacySeasonList.add(LegacySeason("Doctor Who", 6, 12, Year.of(2011), 13, LocalDate.parse("2020-02-18"), "400"))
+        legacySeasonList.add(LegacySeason("Earth 2", 1, 1, Year.of(1994), 22, LocalDate.parse("2020-02-18"), "500"))
+        legacySeasonList.add(LegacySeason("Firefly", 1, 1, Year.of(2002), 14, LocalDate.parse("2020-02-18"), "600"))
+        legacySeasonList.add(LegacySeason("Gotham", 4, 5, Year.of(2017), 22, LocalDate.parse("2020-02-18"), "700"))
     }
 
     fun getSeasonById(id: String): LegacySeason? {

77cb93133e96560e5be04346aca940fe973ca576
Merlin Thomas <emteeware@gmail.com>
2020-06-27 18:23:25 +0200

    [#6] Test full set of properties for one season

--- src/test/kotlin/net/emteeware/SeasonEndpointTest.kt
+++ src/test/kotlin/net/emteeware/SeasonEndpointTest.kt
@@ -30,6 +30,10 @@ class SeasonEndpointTest {
             .statusCode(HttpStatus.SC_OK).extract().response()
         val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
         assertEquals("Doctor Who", htmlPath.getString("html.body.div.p.find{ it.@id == 'title-400' }"))
+        assertEquals("Staffel 6", htmlPath.getString("html.body.div.p.find{ it.@id == 'count-400' }"))
+        assertEquals("Erstausstrahlung: 2011", htmlPath.getString("html.body.div.p.find{ it.@id == 'year-400' }"))
+        assertEquals("13 Episoden", htmlPath.getString("html.body.div.p.find{ it.@id == 'episodes-400' }"))
+        assertEquals("", htmlPath.getString("html.body.div.p.find{ it.@id == 'lastseen-400' }"))
     }
 
 }

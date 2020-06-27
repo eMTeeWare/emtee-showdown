76a11f951c806977b1beb034b122ea6dfffdaa63
Merlin Thomas <emteeware@gmail.com>
2020-06-27 13:38:44 +0200

    [#6] Add test to verify page title

---
+++ src/test/kotlin/net/emteeware/SeasonEndpointTest.kt
@@ -0,0 +1,24 @@
+package net.emteeware
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import io.restassured.path.xml.XmlPath
+import org.apache.http.HttpStatus
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class SeasonEndpointTest {
+
+    @Test
+    fun verifyPageTitle() {
+        val response = given()
+            .`when`().get("/")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and().contentType(ContentType.HTML).extract().response()
+        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
+        assertEquals("eMTee Showdown", htmlPath.getString("html.head.title"))
+    }
+}

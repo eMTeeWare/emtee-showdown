3e6e115ce876496ea583a456b8ac5e93c51b3372
redcrafter502 <oskar.bahr.lg@gmail.com>
2021-05-29 14:33:04 +0200

    emtee-showdown-78 Add option to let dark mode follow system settings

--- src/main/resources/META-INF/resources/settings/load-settings.js
+++ src/main/resources/META-INF/resources/settings/load-settings.js
@@ -1,9 +1,19 @@
 function loadDarkmode(settings) {
-    const isDarkModeActive = settings.dark_mode;
-    if(isDarkModeActive) {
-        document.body.classList.add("dark-mode");
+    const darkmode = settings?.dark_mode ? settings?.dark_mode : 'auto';
+
+    const loadDarkmode = (isDarkmodeOn) => {
+        if (isDarkmodeOn) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }
+
+    if(darkmode === 'auto') {
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => loadDarkmode(event.matches));
+        loadDarkmode(window.matchMedia('(prefers-color-scheme: dark)').matches);
     } else {
-        document.body.classList.remove("dark-mode");
+        loadDarkmode(darkmode === 'on');
     }
 }
 
@@ -11,3 +21,9 @@ function loadSettings() {
     const settings = JSON.parse(window.localStorage.getItem('settings'));
     loadDarkmode(settings);
 }
+
+window.addEventListener('storage', () => {
+   loadSettings();
+});
+
+loadSettings();
--- src/main/resources/META-INF/resources/settings/settings.js
+++ src/main/resources/META-INF/resources/settings/settings.js
@@ -1,15 +1,48 @@
-function changeDarkMode() {
+const darkmodeSystemInputElement = document.getElementById('darkmodeSystem');
+const darkmodeOnInputElement = document.getElementById('darkmodeOn');
+const darkmodeOffInputElement = document.getElementById('darkmodeOff');
+
+/*function changeDarkMode() {
     const inputElement = document.getElementById("dark-mode-input-element");
     const isDarkModeActive = inputElement.checked;
     const settingsValue = {
         dark_mode: isDarkModeActive
     }
     window.localStorage.setItem('settings', JSON.stringify(settingsValue));
+}*/
+function changeDarkmode() {
+    let darkmode = null;
+    if (darkmodeSystemInputElement.checked) {
+        darkmode = 'auto';
+    }
+    if (darkmodeOnInputElement.checked) {
+        darkmode = 'on'
+    }
+    if (darkmodeOffInputElement.checked) {
+        darkmode = 'off'
+    }
+    const settingsValue = {
+        dark_mode: darkmode
+    };
+    window.localStorage.setItem('settings', JSON.stringify(settingsValue));
     loadSettings();
 }
 
 function load() {
-    const inputElement = document.getElementById("dark-mode-input-element");
-    inputElement.checked = JSON.parse(window.localStorage.getItem("settings")).dark_mode
-    loadSettings()
+    const darkmode = JSON.parse(window.localStorage.getItem('settings'))?.dark_mode || null;
+    if(darkmode === 'auto' || darkmode === null) {
+        darkmodeSystemInputElement.click();
+    }
+    if(darkmode === 'on') {
+        darkmodeOnInputElement.click();
+    }
+    if(darkmode === 'off') {
+        darkmodeOffInputElement.click();
+    }
 }
+
+window.addEventListener('storage', () => {
+    load();
+});
+
+load();

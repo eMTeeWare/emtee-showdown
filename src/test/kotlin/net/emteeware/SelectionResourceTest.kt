521a740ab89649b20f036a52022454896950b14f
Merlin Thomas <emteeware@gmail.com>
2021-05-30 17:23:00 +0200

    [#7] Test for correct page title

--- src/test/kotlin/net/emteeware/SelectionResourceTest.kt
+++ src/test/kotlin/net/emteeware/SelectionResourceTest.kt
@@ -2,7 +2,10 @@ package net.emteeware
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import io.restassured.path.xml.XmlPath
 import org.apache.http.HttpStatus
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -16,4 +19,18 @@ class SelectionResourceTest {
             .statusCode(HttpStatus.SC_NOT_FOUND)
     }
 
+    @Test
+    fun `verify page title`() {
+        val response = given()
+            .`when`().get("/selection")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract().response()
+        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
+        assertEquals("eMTee Showdown - Selection", htmlPath.getString("html.head.title"))
+    }
+
+
+
 }
+

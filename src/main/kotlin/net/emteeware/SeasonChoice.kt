3a4def7f460240c65dbad6bb14d7ce269e0acb09
Merlin Thomas <emteeware@gmail.com>
2021-05-30 19:54:05 +0200

    [#87] Reset season list after update test to not to interfere with other tests

--- src/main/kotlin/net/emteeware/SeasonChoice.kt
+++ src/main/kotlin/net/emteeware/SeasonChoice.kt
@@ -9,14 +9,7 @@ class SeasonChoice {
     val legacySeasonList: ArrayList<LegacySeason> = ArrayList()
 
     init {
-        legacySeasonList.add(LegacySeason("ALF", 2, 4, Year.of(1987), 26, LocalDate.parse("2020-03-08"), "100"))
-        legacySeasonList.add(
-            LegacySeason("Battlestar Galactica", 4, 4, Year.of(2008), 20, LocalDate.parse("2020-03-08"), "200"))
-        legacySeasonList.add(LegacySeason("Captain Future", 1, 14, Year.of(1978), 52, LocalDate.parse("2020-02-18"), "300"))
-        legacySeasonList.add(LegacySeason("Doctor Who", 6, 12, Year.of(2011), 13, LocalDate.parse("2020-02-18"), "400"))
-        legacySeasonList.add(LegacySeason("Earth 2", 1, 1, Year.of(1994), 22, LocalDate.parse("2020-02-18"), "500"))
-        legacySeasonList.add(LegacySeason("Firefly", 1, 1, Year.of(2002), 14, LocalDate.parse("2020-02-18"), "600"))
-        legacySeasonList.add(LegacySeason("Gotham", 4, 5, Year.of(2017), 22, LocalDate.parse("2020-02-18"), "700"))
+        reset()
     }
 
     fun getSeasonById(id: String): LegacySeason? {
@@ -31,6 +24,18 @@ class SeasonChoice {
             false
         }
 
+    }
+
+    fun reset() {
+        legacySeasonList.clear()
+        legacySeasonList.add(LegacySeason("ALF", 2, 4, Year.of(1987), 26, LocalDate.parse("2020-03-08"), "100"))
+        legacySeasonList.add(
+            LegacySeason("Battlestar Galactica", 4, 4, Year.of(2008), 20, LocalDate.parse("2020-03-08"), "200"))
+        legacySeasonList.add(LegacySeason("Captain Future", 1, 14, Year.of(1978), 52, LocalDate.parse("2020-02-18"), "300"))
+        legacySeasonList.add(LegacySeason("Doctor Who", 6, 12, Year.of(2011), 13, LocalDate.parse("2020-02-18"), "400"))
+        legacySeasonList.add(LegacySeason("Earth 2", 1, 1, Year.of(1994), 22, LocalDate.parse("2020-02-18"), "500"))
+        legacySeasonList.add(LegacySeason("Firefly", 1, 1, Year.of(2002), 14, LocalDate.parse("2020-02-18"), "600"))
+        legacySeasonList.add(LegacySeason("Gotham", 4, 5, Year.of(2017), 22, LocalDate.parse("2020-02-18"), "700"))
     }
 
 

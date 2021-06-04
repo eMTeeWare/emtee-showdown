3834342c32ac32159fbeb59f31e8421646d08017
Merlin Thomas <emteeware@gmail.com>
2021-06-04 17:31:55 +0200

    [#89] Extract html extraction to method

--- src/test/kotlin/net/emteeware/SelectionResourceTest.kt
+++ src/test/kotlin/net/emteeware/SelectionResourceTest.kt
@@ -13,44 +13,46 @@ class SelectionResourceTest {
 
     @Test
     fun `verify page title`() {
-        val response = getSeasons()
-        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
-        assertEquals("eMTee Showdown - Selection", htmlPath.getString("html.head.title"))
+        val html = getHtml()
+        assertEquals("eMTee Showdown - Selection", html.getString("html.head.title"))
     }
 
     @Test
     fun `verify that no seasons are selected by default`() {
-        val response = getSeasons()
-        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
-        assertEquals("", htmlPath.getString("html.body"))
+        val html = getHtml()
+        assertEquals("", html.getString("html.body"))
     }
 
     @Test
     fun `verify that season appears in selection when it is selected`() {
         postSeason("100", "user1")
-        val response = getSeasons()
-        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
-        assertEquals("ALF", htmlPath.getString("html.body.div.div.div[1].div.b"))
+        val html = getHtml()
+        assertEquals("ALF", html.getString("html.body.div.div.div[1].div.b"))
     }
 
     @Test
     fun `verify that unselected season is removed from selection`() {
         postSeason("100", "user1")
         deleteSeason("100", "user1")
-        val response = getSeasons()
-        val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
-        assertEquals("", htmlPath.getString("html.body"))
+        val html = getHtml()
+        assertEquals("", html.getString("html.body"))
     }
 
     @Test
     fun `verify that a season appears twice on the selection page if two users select it`() {
         postSeason("100", "user1")
         postSeason("100", "user2")
+        val html = getHtml()
+        assertEquals("ALF", html.getString("html.body.div[0].div.div[1].div.b"))
+        assertEquals("ALF", html.getString("html.body.div[1].div.div[1].div.b"))
+    }
+
+    private fun getHtml(): XmlPath {
         val response = getSeasons()
         val htmlPath = XmlPath(XmlPath.CompatibilityMode.HTML, response.body.asString())
-        assertEquals("ALF", htmlPath.getString("html.body.div[0].div.div[1].div.b"))
-        assertEquals("ALF", htmlPath.getString("html.body.div[1].div.div[1].div.b"))
+        return htmlPath
     }
+
 
     private fun deleteSeason(seasonId: String, userId: String) {
         given()

e39562a5103aa4d51b5de80fc98e7c65a593d019
Merlin Thomas <emteeware@gmail.com>
2021-06-05 18:00:12 +0200

    [#85] Add hints how to run the native test

--- src/test/kotlin/net/emteeware/NativeIndexPageTestIT.kt
+++ src/test/kotlin/net/emteeware/NativeIndexPageTestIT.kt
@@ -2,5 +2,11 @@ package net.emteeware
 
 import io.quarkus.test.junit.NativeImageTest
 
+// How to run
+/* Running this test from the IDE might fail on Windows as building the native application might fail.
+Build the native app manually as described here:  https://quarkus.io/guides/building-native-image
+Provide the necessary application properties as environment variables, e. g. via a run configuration.
+ */
+
 @NativeImageTest
 class NativeIndexPageTestIT : IndexPageTest()

560afbb1a89940785e8c0b2bece1a3c3b34d6eeb
Merlin Thomas <emteeware@gmail.com>
2022-01-31 17:27:02 +0100

    [#94] Read dependencies from xml file and display them on "legal" endpoint

--- src/main/kotlin/net/emteeware/DependencyList.kt
+++ src/main/kotlin/net/emteeware/DependencyList.kt
@@ -4,14 +4,5 @@ import javax.inject.Singleton
 
 @Singleton
 class DependencyList {
-    val dependencyList: ArrayList<Dependency> = ArrayList()
-
-    init {
-        reset()
-    }
-
-    private fun reset() {
-        dependencyList.add(Dependency(1,"Test dependency 1"))
-        dependencyList.add(Dependency(2,"Test dependency 2"))
-    }
+    var dependencies = mutableListOf<Dependency>()
 }
--- src/main/kotlin/net/emteeware/LegalResource.kt
+++ src/main/kotlin/net/emteeware/LegalResource.kt
@@ -1,12 +1,15 @@
 package net.emteeware
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import io.quarkus.qute.Template
 import io.quarkus.qute.TemplateInstance
+import java.io.File
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import com.fasterxml.jackson.module.kotlin.readValue
 
 @Path("/legal")
 class LegalResource {
@@ -14,14 +17,12 @@ class LegalResource {
     @Inject
     lateinit var legal: Template
 
-    @Inject
-    lateinit var dependencies: DependencyList
-
-
     @GET
     @Produces(MediaType.TEXT_HTML)
     fun legal(): TemplateInstance {
-        return legal.data("dependency", dependencies.dependencyList)
+        val xml = File(javaClass.classLoader.getResource("attribution.xml")!!.file)
+        val mapper = XmlMapper()
+        val dependencyList : DependencyList = mapper.readValue(xml)
+        return legal.data("dependency", dependencyList.dependencies)
     }
-
 }

dccf4fdc398987c14af40a82daa6ac427df495c3
Merlin Thomas <emteeware@gmail.com>
2022-01-31 19:44:49 +0100

    [#94] Make dependency list accessible from within jar

--- src/main/kotlin/net/emteeware/LegalResource.kt
+++ src/main/kotlin/net/emteeware/LegalResource.kt
@@ -3,7 +3,6 @@ package net.emteeware
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import io.quarkus.qute.Template
 import io.quarkus.qute.TemplateInstance
-import java.io.File
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -20,7 +19,7 @@ class LegalResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     fun legal(): TemplateInstance {
-        val xml = File(javaClass.classLoader.getResource("attribution.xml")!!.file)
+        val xml = javaClass.classLoader.getResourceAsStream("/META-INF/resources/attribution.xml")!!
         val mapper = XmlMapper()
         val dependencyList : DependencyList = mapper.readValue(xml)
         return legal.data("dependency", dependencyList.dependencies)

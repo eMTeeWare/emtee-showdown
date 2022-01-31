c63b02c10ffbcbe676948eac1fc214756b85f3ee
Merlin Thomas <emteeware@gmail.com>
2022-01-31 17:27:02 +0100

    [#94] Read dependencies from xml file and display them on "legal" endpoint

--- src/main/kotlin/net/emteeware/Dependency.kt
+++ src/main/kotlin/net/emteeware/Dependency.kt
@@ -1,9 +1,21 @@
 package net.emteeware
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.quarkus.qute.TemplateData
 
 @TemplateData
 data class Dependency(
-    val id: Int,
-    val name: String
+    @JsonProperty("name") val name: String,
+    @JsonProperty("groupId") val groupId: String,
+    @JsonProperty("artifactId") val artifactId: String,
+    @JsonProperty("version") val version: String,
+    @JsonProperty("projectUrl") val projectUrl: String,
+    @JsonProperty("type") val type: String,
+    @JsonProperty("licenses") val licenses: MutableList<License>,
+    @JsonProperty("downloadUrls") val downloadUrls: MutableList<String>,
+)
+
+data class License(
+    @JsonProperty("name") val name: String,
+    @JsonProperty("url") val url: String,
 )

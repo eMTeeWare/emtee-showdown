d30726c11df80dc43f689ca76c9f719a7ce3eef9
Merlin Thomas <emteeware@gmail.com>
2022-07-03 00:53:20 +0200

    [#107] Read and display properties
    It is only on the legal page for now as the main page only shows static content.

--- src/main/kotlin/net/emteeware/LegalResource.kt
+++ src/main/kotlin/net/emteeware/LegalResource.kt
@@ -1,14 +1,15 @@
 package net.emteeware
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.quarkus.qute.Template
 import io.quarkus.qute.TemplateInstance
+import java.util.*
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
-import com.fasterxml.jackson.module.kotlin.readValue
 
 @Path("/legal")
 class LegalResource {
@@ -19,9 +20,16 @@ class LegalResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     fun legal(): TemplateInstance {
+        val propertyFile = this.javaClass.getResourceAsStream("/META-INF/resources/properties")!!
+        val p = Properties()
+        p.load(propertyFile)
+        val timestamp = p.getProperty("display.timestamp")
+        val version = p.getProperty("display.version")
+
         val xml = javaClass.classLoader.getResourceAsStream("/META-INF/resources/attribution.xml")!!
         val mapper = XmlMapper()
         val dependencyList : DependencyList = mapper.readValue(xml)
-        return legal.data("dependency", dependencyList.dependencies)
+
+        return legal.data("dependency", dependencyList.dependencies).data("version", version).data("timestamp", timestamp)
     }
 }

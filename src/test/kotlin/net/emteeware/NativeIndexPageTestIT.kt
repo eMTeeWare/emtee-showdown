e10580c649249f933755036e08062269d7e07c8a
Merlin Thomas <emteeware@gmail.com>
2022-07-02 21:19:22 +0200

    [#108] disable native test for the time being

--- src/test/kotlin/net/emteeware/NativeIndexPageTestIT.kt
+++ src/test/kotlin/net/emteeware/NativeIndexPageTestIT.kt
@@ -1,6 +1,7 @@
 package net.emteeware
 
 import io.quarkus.test.junit.QuarkusIntegrationTest
+import org.junit.jupiter.api.Disabled
 
 // How to run
 /* Running this test from the IDE might fail on Windows as building the native application might fail.
@@ -8,5 +9,6 @@ Build the native app manually as described here:  https://quarkus.io/guides/buil
 Provide the necessary application properties as environment variables, e. g. via a run configuration.
  */
 
+@Disabled
 @QuarkusIntegrationTest
 class NativeIndexPageTestIT : IndexPageTest()

84127a8b486b35590e91bc19686ba8973fa8c132
oskar <oskar.bahr.lg@gmail.com>
2024-05-06 21:01:43 +0200

    [#102] Added foldout feature to selection page

--- src/main/resources/META-INF/resources/selection.js
+++ src/main/resources/META-INF/resources/selection.js
@@ -11,5 +11,20 @@ const flip = (id, selected) => {
 }
 const randomFlip = selected => flip(getApprovedSelections()[Math.floor(Math.random()*getApprovedSelections().length)].children[0].id, selected)
 
+const summaries = document.getElementsByClassName("summary")
+Array.from(summaries).forEach(summary => {
+    summary.style.height = 150 - summary.parentElement.children[0].scrollHeight - summary.parentElement.children[1].scrollHeight - summary.parentElement.children[2].scrollHeight - 34 + "px"
+
+    summary.addEventListener("mouseover", () => {
+        summary.style.height = summary.scrollHeight + "px"
+        summary.parentElement.parentElement.parentElement.parentElement.style.height = summary.scrollHeight + summary.parentElement.children[0].scrollHeight + summary.parentElement.children[1].scrollHeight + summary.parentElement.children[2].scrollHeight + 34 + "px"
+    })
+
+    summary.addEventListener("mouseout", () => {
+        summary.style.height = 150 - summary.parentElement.children[0].scrollHeight - summary.parentElement.children[1].scrollHeight - summary.parentElement.children[2].scrollHeight - 34 + "px"
+        summary.parentElement.parentElement.parentElement.parentElement.style.height = "150px"
+    })
+})
+
 flipSelectedButton.addEventListener("click", () => randomFlip(true))
 flipDiscardedButton.addEventListener("click", () => randomFlip(false))

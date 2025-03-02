e41f927086e0b3100b074dfafeec67b5654a1c81
Merlin Thomas <emteeware@gmail.com>
2025-03-02 16:03:11 +0100

    [#77] Adjust tests after changing update page

--- src/test/kotlin/net/emteeware/UpdateResourceTest.kt
+++ src/test/kotlin/net/emteeware/UpdateResourceTest.kt
@@ -9,7 +9,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
-import org.skyscreamer.jsonassert.JSONAssert
+import org.junit.jupiter.api.Assertions.assertEquals
 
 import java.time.Instant
 import java.util.*
@@ -59,31 +59,91 @@ internal class UpdateResourceTest {
     @Test
     fun update() {
         val expectedJson =
-            """{
-    "legacySeasonList": [
-        {
-            "id": "42",
-            "lastSeen": "-999999999-01-01",
-            "localizedTitle": "Die Tester",
-            "number": 3,
-            "numberOfEpisodes": 14,
-            "selected": false,
-            "show": "The Testers",
-            "summary": "Fry und Bender testen neue Möglichkeiten Geld zu verdienen",
-            "totalSeasons": 0,
-            "yearOfFirstAiring": {
-                "leap": false,
-                "value": 2021
-            }
-        }
-    ]
-}"""
+            """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>eMTee Showdown - Seasons</title>
+    <link rel="stylesheet" href="seasons.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+    <link rel="stylesheet" href="/settings/dark-mode.css" />
+    <script type="application/javascript" src="seasons.js" defer></script>
+    <script src="settings/load-settings.js" defer></script>
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="64x64"
+        href="emtee-showdown-icon.png"
+    />
+</head>
+<body>
+<div class="top-of-page">
+<nav class="nav">
+  <ul class="nav-ul">
+    <li><img src="emtee-showdown-icon.png" alt="emtee-showdown logo" /></li>
+    <li ><a href="/index.html">Home</a></li>
+    <li class="nav-selected"><a href="/update">Update</a></li>
+    <li ><a href="/seasons">Seasons</a></li>
+    <li ><a href="/selection">Selection</a></li>
+    <li ><a href="/settings/index.html">Settings</a></li>
+    <li ><a href="/legal">Legal</a></li>
+  </ul>
+</nav>
+<style>
+  .nav {
+    width: 100%;
+    height: 3rem;
+  }
+  .nav img {
+    padding: 7px 8px;
+    max-height: 34px; /* 3rem (48px|3*16px) - 14px|2*7px */
+  }
+  .nav a {
+    color: white;
+  }
+  .nav-selected a {
+    background-color: #48cbdd;
+  }
+  .nav a:visited {
+    color: white;
+  }
+  .nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    height: 100%;
+  }
+  .nav li {
+    float: left;
+  }
+  .nav a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  .nav a:hover {
+    background-color: #111;
+  }
+  .nav-selected a:hover {
+    background-color: #00A1E2;
+  }
+</style>
+</div>
+<div class="updated" id="updated" style="padding: 15px">
+    1 seasons updated from Trakt.
+</div>
+</body>
+</html>"""
         every { traktListService.update( any(), any(), any(), any(), any() ) } returns traktEntries
         every { traktShowService.getTranslation( any(), any(), any(), any() ) } returns traktShows
         val response = given().log().all().get("/update")
         response.then().assertThat().statusCode(200)
         println(response.body.asString())
-        JSONAssert.assertEquals(expectedJson, response.body.asString(), false)
+        assertEquals(expectedJson, response.body.asString())
 
     }
 
